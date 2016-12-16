33b3754ea7353ed8f9944494cd09741c025aff47
Ryan B <admirallasers@gmail.com>
2016-12-15 19:36:42 -0600

    hit an impasse and committing to ask help

---
+++ PracticeBuilder/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('practiceBuilder', ['ngRoute']);
+
+/*
+app.controller("PracticeCtrl", function ($scope, $http) {
+
+});
+
+app.controller("SlideCtrl", function ($scope, $http) {
+
+});*/
